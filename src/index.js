b7af8c70e4136690d05142baa31de7be54e98da6
Laiba Waheed <laibawaheed122@gmail.com>
2025-12-10 15:07:57 +0500

    Updated file with new changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import express from "express";
 import productsRouter from "./routes/products.js";
 
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(express.json());
 
 app.use("/api/products", productsRouter);
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
